6feec63db228bdb3f41a3b07fa3b889ccc3128d5
feranmi <feranmiolasunkanmi91@gmail.com>
2024-01-09 18:44:40 +0100

    calculating balance

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -11,22 +11,12 @@
       </ul>
 </template>
 <script setup>
-  // export default {
-  //   // data() {
-  //   //   return {
-  //   //     transactions: [
-  //   //       { id: 1, text: 'Flower', amount: -19.99 },
-  //   //       { id: 2, text: 'Salary', amount: 299.99 },
-  //   //       { id: 3, text: 'Book', amount: -10  },
-  //   //       { id: 4, text: 'Camera', amount: 150 },
-  //   //     ]
-  //   //   }
-  //   // }
-  // }
-  const transactions = [
-          { id: 1, text: 'Flower', amount: -19.99 },
-          { id: 2, text: 'Salary', amount: 299.99 },
-          { id: 3, text: 'Book', amount: -10  },
-          { id: 4, text: 'Camera', amount: 150 },
-        ]
+  import { defineProps } from 'vue';
+
+  const props = defineProps({
+    transactions: {
+      type: Array,
+      required: true
+    }
+  })
 </script>
