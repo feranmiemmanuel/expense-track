4e5f2cb4ad9889d79cd6b8e5eb026d3a84a97127
feranmi <feranmiolasunkanmi91@gmail.com>
2024-01-11 12:54:02 +0100

    working on edit

---
+++ src/components/EditTransaction.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="box">
+            <div class="box-inner">
+                <h3>Edit transaction</h3>
+                    <form id="form" @submit.prevent="">
+                        <div class="form-control">
+                        <label for="text">Text</label>
+                        <input v-model="transaction.text" type="text" id="text" placeholder="Enter text..." />
+                        </div>
+                        <div class="form-control">
+                        <label for="amount"
+                            >Amount <br />
+                            (negative - expense, positive - income)</label
+                        >
+                        <input v-model="transaction.amount" type="text" id="amount" placeholder="Enter amount..." />
+                        </div>
+                        <button class="btn">Add transaction</button>
+                    </form>
+                </div>
+            <!-- box-inner -->
+        </div>
+        <!-- box -->
+</template>
+<script setup>
+ const emit = defineEmits(['transactionEdit'])
+  const props = defineProps({
+    transaction: {
+      type: Object,
+      required: true
+    }
+  })
+</script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <h3>History</h3>
       <ul id="list" class="list">
         <li
@@ -8,11 +9,33 @@
         >
           {{ transaction.text }} <span>${{ transaction.amount }}</span>
           <button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
+          <button
+          @click="() => {
+             showEdit = !showEdit
+             editData(transaction)
+          }"
+          class="edit-btn"
+          ><i class="bi bi-pencil-square"></i></button>
         </li>
       </ul>
+
+      <div class="overlay" v-if="showEdit">
+      </div>
+
+      <div class="modal" v-if="showEdit">
+        <button @click="
+          myShow();
+          " class="modal-close btn btn_small">
+          <i class="icn icn_close">x</i>
+        </button>
+        <EditTransaction :transaction="transactionval"/>
+      </div>
+  </div>
 </template>
 <script setup>
   import { defineProps } from 'vue';
+  import EditTransaction from './EditTransaction.vue'
+  import { ref } from "vue"
 
   const emit = defineEmits(['transactionDeleted'])
   const props = defineProps({
@@ -21,7 +44,21 @@
       required: true
     }
   })
+  let transactionval = ref([]);
+  const showEdit = ref(false);
+  const editData = (transaction) => {
+    // transaction
+    transactionval = transaction;
+    return transactionval;
+  }
   const deleteTransaction = (transactionId) => {
     emit('transactionDeleted', transactionId)
   }
+  const myShow = () => {
+    showEdit.value = false;
+    console.log("clicked");
+  }
+  // const editTransaction = (transactionId) => {
+  //   emit('transactionEdit', transactionId)
+  // }
 </script>
